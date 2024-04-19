295ad82b240949002e52749c9556afd049fd5d74
Davi Santos <daviir17@gmail.com>
2024-04-19 03:36:21 -0300

    Finalizei a estrutura dos cards de certificados

--- src/components/CardCertification/index.jsx
+++ src/components/CardCertification/index.jsx
@@ -1,9 +1,38 @@
 import './styles.css';
+/* import react from '../../assets/react.svg' */
 
-export default function CardCertification() {
+export default function CardCertification({img, name, description, institution, conclusion, duration, style_icone={}, style_title={}}) {
   return (
     <div className='cardCertification-container'>
-      
+      <div className='image-container-certification'>
+        <div className='image-content-certification'>
+          <img style={style_icone} src={img} alt="" />
+        </div>
+      </div>
+      <div className='description-certification-container'>
+        <h1 style={style_title}>{name}</h1>
+        <span className='description'>{description}</span>
+        <div className='institution-name-container'>
+          <span className='institution-title'>Instituição:</span>
+          <p className='institution-name'>{institution}</p>
+        </div>
+
+        <div className='institution-name-container'>
+          <span className='institution-title'>Conclusão:</span>
+          <p className='institution-name'>{conclusion}</p>
+        </div>
+
+        <div className='institution-name-container'>
+          <span className='institution-title'>Duração:</span>
+          <p className='institution-name'>{duration}</p>
+        </div>
+
+        <div className='credential-btn'>
+          <p>Exibir credencial</p>
+          <span class="material-symbols-outlined">ios_share</span>
+        </div>
+        {/* <h1>Como criar e modificar um projeto Git</h1> */}
+      </div>
     </div>
   )
 }
--- src/pages/Certifications/index.jsx
+++ src/pages/Certifications/index.jsx
@@ -1,11 +1,80 @@
 import CardCertification from '../../components/CardCertification';
+import git from '../../assets/git.png';
+import react from '../../assets/react.svg';
+import javascript from '../../assets/javascript.png';
+import htmlCss from '../../assets/html-css.png';
+import python from '../../assets/python.png';
+import wordpress from '../../assets/wordpress.webp';
+
 import './styles.css';
 
 export default function Certifications() {
   return (
     <div className='certifications-container defaultPages'>
       <h1>Licenças e certificados</h1>
-      <CardCertification/>
+      <div className='certifications-content'>
+        <CardCertification 
+          img={git} 
+          name={'git'} 
+          description={'Introdução ao Git'}
+          institution={'Microsoft Learn'}
+          conclusion={'2024'}
+          duration={'---'}
+        />
+        <CardCertification 
+          img={git} 
+          name={'git'} 
+          description={'Como criar e modificar um projeto Git'}
+          institution={'Microsoft Learn'}
+          conclusion={'2024'}
+          duration={'---'}
+        />
+        <CardCertification 
+          img={react} 
+          name={'React'}
+          description={'React.Js e Next.Js (Intermediário e Avançado)'}
+          institution={'Udemy'}
+          conclusion={'2024'}
+          duration={'146 horas'}
+          style_icone={{width: '60px', height: '70px'}}
+        />
+        <CardCertification 
+          img={javascript} 
+          name={'JavaScript'} 
+          description={'JavaScript e TypeScript do básico ao avançado'}
+          institution={'Udemy'}
+          conclusion={'2024'}
+          duration={'93 horas'}
+          style_icone={{width: '30px', height: '30px', border: '1px solid #c7c6bf'}}
+        />
+
+        <CardCertification
+          img={htmlCss}
+          name={'HTML5 e CSS3'}
+          description={'Do básico ao avançado'}
+          institution={'Curso em vídeo'}
+          conclusion={'2023'}
+          duration={'160 horas'}
+          style_title={{fontSize: '1.4em'}}
+        />
+        <CardCertification
+          img={python}
+          name={'Python'}
+          description={'Do básico ao avançado'}
+          institution={'Curso em vídeo'}
+          conclusion={'2022'}
+          duration={'120 horas'}
+        />
+        <CardCertification
+          img={wordpress}
+          name={'Wordpress'}
+          description={'Wordpress com gutenberg'}
+          institution={'Estudonauta'}
+          conclusion={'2021'}
+          duration={'80 horas'}
+          style_icone={{width: '45px', height: '45px'}}
+        />
+      </div>
     </div>
   )
 }
