6c7e276c863b0803f8217105e606b82bc1f69ac6
Davi Santos <daviir17@gmail.com>
2024-05-11 01:53:34 -0300

    emplementação da feature de troca de header na rolagem do scroll

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Skills from './pages/Skills';
 function App() {
 
   return (
+
     <div className='app-container'>
       <Headers/>
       <FloatingButtons/>
@@ -21,6 +22,7 @@ function App() {
       </div>
       <Footer/>
     </div>
+
   )
 }
 
--- src/components/Headers/index.jsx
+++ src/components/Headers/index.jsx
@@ -1,17 +1,30 @@
+import { useState } from 'react';
+import useEventListener from '../../hooks/useEventListenerDocument';
 import Header from '../Header';
 import HeaderScroll from '../HeaderScroll';
 import './styles.css';
 
+
+const viewHight = window.innerHeight
+
 export default function Headers() {
+
+  const [ scrolbarPositionWin, setScrolbarPositionWin ] = useState(0);
+
+  const handleOnScrollWin = () => {
+    setScrolbarPositionWin(window.scrollY);
+  }
+
+  useEventListener('scroll', handleOnScrollWin);
+
   return (
     <>
-      {false &&
-        <Header/>
-      }
+      <Header/>
 
-      {true &&
+      {scrolbarPositionWin > (viewHight - 80) &&
         <HeaderScroll/>
       }
+      
     </>
   )
 }
