2a2b6aefa6e0068a83d1be8a3945f772d4b476f4
Davi Santos <daviir17@gmail.com>
2024-06-03 17:39:06 -0300

    finalização do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,31 +1,49 @@
+import { Link } from 'react-scroll';
 import Logo from '../Logo';
 import './styles.css';
 
 export default function Footer() {
   return (
     <footer className='footer-container'>
-      <div className='footer-content-social-links'>
-        <div className='footer-logo-container'>
-          <Logo/>
-        </div>
-        <div className='footer-social-container'>
-          <div className='footer-social-btn'>
-           <i className='bx bxl-linkedin-square' />
-           <span>Linkedin</span>
-          </div>
-          <div className='footer-social-btn'>
-           <i className='bx bxl-github' />
-           <span>Github</span>
-          </div>
-      
-          <div className='footer-social-btn'>
-           <i className='bx bxl-instagram' />
-           <span>Instagram</span>
-          </div>
-        </div>
-      </div>
       <div className='footer-content'>
-        <span>Desenvolvido por <a target='_blank' href="https://www.linkedin.com/in/davisantoss/">Davi Santos</a></span>
+
+        <div className='social-btns-footer'>
+        <i class='bx bxl-linkedin' ></i>
+        <i class='bx bxl-github' ></i>
+        <i class='bx bxl-whatsapp' ></i>
+        </div>
+
+        <nav className='navigation-footer' >
+
+          <Link to='id_header' smooth={true} className='Link-footer'>
+            <span>Início</span>
+          </Link>
+
+          <Link to='id_certifications' smooth={true} offset={-79} duration={700} className='Link-footer'>
+            <span>Certificações</span>
+          </Link>
+
+          <Link to='id_skills' smooth={true} offset={-79} duration={700} className='Link-footer'>
+            <span>Habilidades</span>
+          </Link>
+
+          <Link to='id_projects' smooth={true} offset={-79} duration={700} className='Link-footer'>
+            <span>Projetos</span>
+          </Link>
+
+          <Link to='id_experience' smooth={true} offset={-79} duration={700} className='Link-footer'>
+            <span>Experiências</span>
+          </Link>
+
+          <Link to=''  className='Link-footer'>
+            <span>Contato</span>
+          </Link>
+
+        </nav>
+
+        <span className='author-footer'>
+          Criado por Davi Santos
+        </span>
       </div>
     </footer>
   )
