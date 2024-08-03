3c564260845098f83279d138bf239cde1dd85ff8
Davi Santos <daviir17@gmail.com>
2024-08-03 01:32:14 -0300

    responsividade: adaptação dos elemntos no header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,30 +3,46 @@ import './styles.css';
 import Button from '../Button';
 import NavHeader from '../NavHeader';
 import DarkModeBtn from '../DarkModeBtn'
+import { useMediaQuery } from 'react-responsive';
 
 export default function Header() {
+
+  const isTabletOrMobile = useMediaQuery({query: '(max-width: 1100px)'});
+  const Mobile = useMediaQuery({query: '(max-width: 500px)'});
+  const miniMobile = useMediaQuery({query: '(max-width: 410px)'});
   
   return (
     <header className='header-container-scroll'>
       <div className='header-content-scroll'>
         <div className='logo-and-buttons-header-scroll'>
-          <Logo/*  headerScroll={true} */ />
-          <div className='navHeaderAnimationBug-container-scroll'>
+          <Logo/>
 
-            <NavHeader/>
+          {!isTabletOrMobile &&
+          
+            <div className='navHeaderAnimationBug-container-scroll'>
 
-          </div>
+              <NavHeader/>
+
+            </div>
+          }
+
           <div className='btns-header-scroll'>
 
-            <div className='contact-btn-scroll'>
+            {!miniMobile &&
+            
+              <div className='contact-btn-scroll'>
 
-                <a className='curriculum-btn-scroll' href="https://drive.google.com/file/d/1hHtHCqwOIouLySi8dBQeAUHaJB8wdku1/view?usp=sharing" target='_blank'>
-                  <Button name={'Currículo'} icon={'file_download'}/>
-                </a>
-              
-            </div>
+                  <a className='curriculum-btn-scroll' href="https://drive.google.com/file/d/1hHtHCqwOIouLySi8dBQeAUHaJB8wdku1/view?usp=sharing" target='_blank'>
+                    <Button name={'Currículo'} icon={'file_download'}/>
+                  </a>
+                
+              </div>
+            
+            }
 
-            <DarkModeBtn styles_container={{marginTop: '0px'}}/>
+            {(!Mobile || miniMobile) &&
+              <DarkModeBtn styles_container={{marginTop: '0px'}}/>
+            }
 
 
           </div>
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,17 +1,31 @@
-import './styles.css';
+import { useMediaQuery } from 'react-responsive'
 import photo from '../../assets/img-perfil.jpeg'
+import './styles.css';
 
 export default function Logo() {
 
+  const isTabletOrMobile = useMediaQuery({query: '(max-width: 1100px)'});
+  
+
   return (
     <div className='logo-container'>
-      <div className='container-photo-logo' style={{width: '40px', height: '40px'}}>
-        <img src={photo} alt=""/>
-      </div>
+
+        {/* se não couber o nav vai desaparecer a foto e aparecer as barras */}
+
+        {
+          isTabletOrMobile && 
+          <span className="material-symbols-outlined menuHeader">menu</span>
+        }
+
+        {!isTabletOrMobile && 
+          <div className='container-photo-logo' style={{width: '40px', height: '40px'}}>
+            <img src={photo} alt=""/>
+          </div>
+        }
 
       <div  className='container-title-logo'>
-        <h1 style={{fontSize: '2em'}}>Portfólio</h1>
-        <span style={{fontSize: '1em'}}>Davi Santos</span>
+        <h1/*  style={{fontSize: '2em'}} */>Portfólio</h1>
+        <span /* style={{fontSize: '1em'}} */>Davi Santos</span>
       </div>
     </div>
   )
