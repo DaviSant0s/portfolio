25098a9a8d01f904f609c476e9275a2fb8d5e527
Davi Santos <daviir17@gmail.com>
2024-08-04 02:14:43 -0300

    fechar a sidebar quando clicar no botao de pagina da sidebar

--- src/components/NavHeaderSideBar/index.jsx
+++ src/components/NavHeaderSideBar/index.jsx
@@ -1,9 +1,16 @@
 import { Link } from 'react-scroll';
 import './styles.css';
+import { useSideBar } from '../../context/SideBarContext';
 import useScrollAnimationSideBar from '../../hooks/header/useScrollAnimationSideBar';
 
 
 export default function NavHeaderSideBar() {
+
+  const { setMenuEnabled } = useSideBar();
+
+  const handleClickCloseSideBar = () => {
+    setMenuEnabled(false)
+  }
 
   useScrollAnimationSideBar();
 
@@ -13,36 +20,42 @@ export default function NavHeaderSideBar() {
       <Link to='id_home' smooth={true} offset={-80} className='Link'>
         <div 
           className='home_sideBar link-page'
+          onClick={handleClickCloseSideBar}
         >Início</div>
       </Link>
 
       <Link to='id_certifications' smooth={true} offset={-79} duration={700} className='Link'>
         <div 
           className='certifications_sideBar link-page'
+          onClick={handleClickCloseSideBar}
         >Certificações</div>
       </Link>
 
       <Link to='id_skills' smooth={true} offset={-79} duration={700} className='Link'>
         <div 
           className='skills_sideBar link-page'
+          onClick={handleClickCloseSideBar}
         >Habilidades</div>
       </Link>
 
       <Link to='id_projects' smooth={true} offset={-79} duration={700} className='Link'>
         <div 
           className='projects_sideBar link-page'
+          onClick={handleClickCloseSideBar}
         >Projetos</div>
       </Link>
 
       <Link to='id_experience' smooth={true} offset={-79} duration={700} className='Link'>
         <div 
           className='experiences_sideBar link-page'
+          onClick={handleClickCloseSideBar}
         >Experiências</div>
       </Link>
 
       <Link to='id_contact'  smooth={true} offset={-79} duration={700} className='Link'>
         <div 
           className='contact_sideBar link-page'
+          onClick={handleClickCloseSideBar}
         >Contato</div>
       </Link>
     </nav>
