64eb1935da4d92210346b82ac55069016551aec0
Davi Santos <daviir17@gmail.com>
2024-08-17 12:57:30 -0300

    responsividade: pagina de experiencia

--- src/pages/Experience/index.jsx
+++ src/pages/Experience/index.jsx
@@ -5,14 +5,14 @@ export default function Experience() {
 
   return (
     <div id='id_experience' className='experience-container defaultPages'>
-      <h1 id='id_title_experience'>
-        Experiências
-        {/* <div style={animationTitle} className='animationTitle'></div> */}
-      </h1>
       <div className='experience-content'>
-
-        <ExperienceContainer/>
+        <h1 id='id_title_experience'>
+          Experiências
+        </h1>
+        <div className='experiences'>
+          <ExperienceContainer/>
         
+        </div>
       </div>
     </div>
   );
